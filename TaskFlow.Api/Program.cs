507b80506ba50459504fac3ffd593a740b1fcb9d
Tidiane Diallo <diallotidian@gmail.om>
2026-02-10 20:12:42 +0100

    feat: initial project structure with clean architecture

--- TaskFlow.Api/Program.cs
+++ TaskFlow.Api/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using TaskFlow.Users.Application.Commands.Register;
